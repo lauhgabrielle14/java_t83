5b81627e1793a7434818f620a043f697e80b768d
lauhgabrielle14 <laurenfogaca_dev@hotmail.com>
2025-09-12 12:44:45 -0300

    Atividades concluídas

--- atividadesLauren/src/atividadesLauren/Atv_java03_java04.java
+++ atividadesLauren/src/atividadesLauren/Atv_java03_java04.java
@@ -31,9 +31,9 @@ public class Atv_java03_java04 {
 		
 		System.out.println("Exercicio 1 \n");
 		
-		System.out.println("Digite o salario: ");
+		System.out.print("Salario: ");
 		salario = leia.nextFloat();
-		System.out.println("Agora digite o abono: ");
+		System.out.print("Abono: ");
 		abono = leia.nextFloat();
 		
 		System.out.printf("Novo salario: %.2f %n%n", salario + abono);
@@ -42,13 +42,13 @@ public class Atv_java03_java04 {
 		
 		System.out.println("Exercicio 2 \n");
 		
-		System.out.println("Nota 1: ");
+		System.out.print("Nota 1: ");
 		nota1 = leia.nextFloat();
-		System.out.println("Nota 2: ");
+		System.out.print("Nota 2: ");
 		nota2 = leia.nextFloat();
-		System.out.println("Nota 3: ");
+		System.out.print("Nota 3: ");
 		nota3 = leia.nextFloat();
-		System.out.println("Nota 4: ");
+		System.out.print("Nota 4: ");
 		nota4 = leia.nextFloat();
 		
 		System.out.printf("Media final: %.1f %n%n", (nota1 + nota2 + nota3 + nota4)/4);
@@ -57,13 +57,13 @@ public class Atv_java03_java04 {
 		
 		System.out.println("Exercicio 3 \n");
 		
-		System.out.println("Salario Bruto: ");
+		System.out.print("Salario Bruto: ");
 		salarioBruto = leia.nextFloat();
-		System.out.println("Adicional Noturno: ");
+		System.out.print("Adicional Noturno: ");
 		adicionalNoturno = leia.nextFloat();
-		System.out.println("Horas Extras: ");
+		System.out.print("Horas Extras: ");
 		horasExtra = leia.nextFloat();
-		System.out.println("Descontos: ");
+		System.out.print("Descontos: ");
 		desconto = leia.nextFloat();
 		
 		System.out.printf("Salario Liquido: %.2f %n %n", salarioBruto + adicionalNoturno + (horasExtra * 5) - desconto);
@@ -72,13 +72,13 @@ public class Atv_java03_java04 {
 		
 		System.out.println("Exercicio 4 \n");
 		
-		System.out.println("numero1: ");
+		System.out.print("numero1: ");
 		n1 = leia.nextFloat();
-		System.out.println("numero2: ");
+		System.out.print("numero2: ");
 		n2 = leia.nextFloat();
-		System.out.println("numero3: ");
+		System.out.print("numero3: ");
 		n3 = leia.nextFloat();
-		System.out.println("numero4: ");
+		System.out.print("numero4: ");
 		n4 = leia.nextFloat();
 		
 		System.out.printf("Diferenca: %.1f", (n1 * n2) - (n3 * n4));
